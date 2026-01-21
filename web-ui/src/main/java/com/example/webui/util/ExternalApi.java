be5f803b6d7f96126eadf0ef6fc6cdb2cc8b2de7
andore htet <hhtetaung578@gmail.com>
2026-01-21 13:30:40 +0630

    Docker setup for microservices app

--- web-ui/src/main/java/com/example/webui/util/ExternalApi.java
+++ web-ui/src/main/java/com/example/webui/util/ExternalApi.java
@@ -2,6 +2,6 @@ package com.example.webui.util;
 
 public class ExternalApi {
 
-    public static final String BACKEND_URL = "http://localhost:8080/";
+    public static final String BACKEND_URL = "http://gatewayservice:8080/";
     public static final String DEPARTMENTS_URL = BACKEND_URL + "department/departments-list";
 }
